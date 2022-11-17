c55a6b88c2418f8b734672027c487e1f2031974d
Edmond <47702632+echaussidon@users.noreply.github.com>
2022-11-17 18:18:32 +0100

    Add truncated distribution for redshift smearing (#19)
    * small update
    
    * add truncated cauchy and normal distribution

--- mockfactory/desi/__init__.py
+++ mockfactory/desi/__init__.py
@@ -1,3 +1,3 @@
 from .brick_pixel_quantities import get_brick_pixel_quantities
 from .footprint import is_in_desi_footprint
-from .redshift_smearing import QSORedshiftSmearing, RedshiftSmearing
+from .redshift_smearing import TracerRedshiftSmearing
